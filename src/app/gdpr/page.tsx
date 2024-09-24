2a6d139709c4d3a423239dbee7435d7cb6f80e0d
Bengo445 <boncosam@gmail.com>
2024-09-24 13:08:27 +0200

    structure updateň

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,13 @@
+// src/app/gdpr/page.tsx
+
+import * as React from 'react';
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export default function GDPR() {
+  return (
+    <Container>
+      <Typography>GDPR</Typography>
+    </Container>
+  );
+}
