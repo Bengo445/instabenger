c864676acbe162655cbdd29571edc293e8229ab1
Bengo445 <boncosam@gmail.com>
2024-10-15 12:43:09 +0200

    navbar update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 // src/app/page.tsx
 
+import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 
 export const metadata = { title: "Home | Instabenger"}
 
 export default function Home() {
   return (
-
+    <Container>
       <Typography>Home</Typography>
-      
+    </Container>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,50 @@
-// src/components/NavBar.tsx
-
 "use client"
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HomeIcon from '@mui/icons-material/Home';
+import PostIcon from '@mui/icons-material/AddToPhotos';
+import LoginIcon from '@mui/icons-material/Login';
+import RegisterIcon from '@mui/icons-material/AppRegistration';
+import { useRouter } from 'next/navigation';
 
 export default function NavBar() {
   const [value, setValue] = React.useState(0);
+  const router = useRouter();
+
+  const handleNavigation = (newValue: number, path: string) => {
+    setValue(newValue);
+    router.push(path);
+  };
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
       <BottomNavigation
         showLabels
         value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction 
+          label="Domov" 
+          icon={<HomeIcon />} 
+          onClick={() => handleNavigation(0, '/')} 
+        />
+        <BottomNavigationAction 
+          label="Príspevok" 
+          icon={<PostIcon />} 
+          onClick={() => handleNavigation(1, '/post')} 
+        />
+        <BottomNavigationAction 
+          label="Registrácia" 
+          icon={<RegisterIcon />} 
+          onClick={() => handleNavigation(2, '/auth/register')} 
+        />
+        <BottomNavigationAction 
+          label="Prihlásenie" 
+          icon={<LoginIcon />} 
+          onClick={() => handleNavigation(3, '/auth/login')} 
+        />
       </BottomNavigation>
     </Box>
   );
